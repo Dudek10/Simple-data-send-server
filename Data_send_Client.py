390ff88bf9b7ee4e7b837a779dfcf327b7a2e147
Pablo Majster <104451887+ppaluchowski64@users.noreply.github.com>
2025-06-26 14:02:40 +0200

    Fix all mistakes

--- Data_send_Client.py
+++ Data_send_Client.py
@@ -3,26 +3,26 @@ import os
 import struct
 import hashlib
 
-HOST = '192.168.0.15'
+HOST = '127.0.0.1'
 PORT = 5000
-
-allowed_extensions = {'jpg', 'jpeg', 'png', 'gif', 'txt'}
+MAX_SIZE = 10485760
+ALLOWED_EXT = {'jpg', 'jpeg', 'png', 'gif', 'txt'}
 
 socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_client.connect((HOST, PORT))
 
+password = input("🔑 Podaj hasło dostępu: ").strip()
+password_hash = hashlib.sha512(password.encode()).digest()
+socket_client.send(password_hash)
+
+response = socket_client.recv(4)
+if response != b"OK": 
+    print("❌ Błędne hasło. Zamykam sesje")
+    socket_client.close()
+    quit()
 
 try:
     while True:
-        password = input("🔑 Podaj hasło dostępu: ").strip()
-        password_hash = hashlib.sha256(password.encode()).digest()
-        socket_client.send(password_hash)
-
-        response = socket_client.recv(4)
-        if response != b"OK": 
-            print("❌ Błędne hasło. Zamykam sesje")
-            break
-
         pathname = input("Podaj nazwę lub ścieżkę pliku do wysłania: ").strip()
         if pathname.lower() == 'exit':
             print("Koniec wysyłania pliku.")
@@ -33,19 +33,22 @@ try:
 
         # sprawdzenie formatu rozszerzenia
         filename = os.path.basename(pathname)
-        _, ext = os.path.splitext(filename)
+        ext = os.path.splitext(filename)[1]
         ext = ext.lower().lstrip('.')
-    
-        if ext not in allowed_extensions:
-            print(f"Format .{ext} nie jest obsługiwany. dozwolone rozszerzenia: {allowed_extensions}")
+
+        if ext not in ALLOWED_EXT:
+            print(f"Format *.{ext} nie jest obsługiwany. Dozwolone rozszerzenia: {ALLOWED_EXT}")
             continue
         socket_client.send(filename.encode())
 
-        #wysyłanie rozmiaru pliku do serwera
         filesize = os.path.getsize(pathname)
-        time.sleep(2)
-        print(f"wysyłanie danych...{filesize}")
-        time.sleep(2)
+
+        if filesize > MAX_SIZE:
+            print("Plik jest za duży. Maksymalny rozmiar to 10MiB")
+            continue
+        
+        #wysyłanie rozmiaru pliku do serwera
+        print(f"Wysyłanie danych...{filesize}")
         socket_client.send(struct.pack('!Q', filesize))
 
         with open(pathname, 'rb') as f:
@@ -54,9 +57,7 @@ try:
                 if not data:
                     break
                 socket_client.send(data)
-            
+
         print(f"Plik {filename} został wysłany.")
 finally:
     socket_client.close()
-
-                
